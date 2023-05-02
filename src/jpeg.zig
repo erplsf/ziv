7713efc479f7973a9ed05b1652c6e94a2cbe5a7f
Andriy Mykhaylyk <erp.lsf@gmail.com>
2023-05-02 19:58:54 +0200

    stash ideas

--- src/jpeg.zig
+++ src/jpeg.zig
@@ -23,12 +23,112 @@ const Parser = struct {
     data: []const u8,
     pos: usize = 0,
 
+    markers: std.AutoHashMap(Marker, usize),
+
     pub fn init(allocator: std.mem.Allocator, data: []const u8) Self {
-        return .{ .allocator = allocator, .data = data };
+        const markers = std.AutoHashMap(Marker, usize).init(allocator);
+        return .{ .allocator = allocator, .data = data, .markers = markers };
     }
 
     pub fn deinit(self: *Self) void {
-        _ = self;
+        self.markers.deinit();
+    }
+
+    pub fn parseMarkers(self: *Self) !void {
+        var i: usize = 0;
+        var soa_found = false;
+
+        var restartInterval: ?u16 = null;
+
+        while (i < self.data.len) : (i += 2) {
+            const value = std.mem.readIntSlice(u16, self.data[i .. i + 2], std.builtin.Endian.Big);
+            const marker = @intToEnum(Marker, value);
+
+            if (soa_found and marker == .endOfImage) {}
+
+            if (!soa_found or marker == .endOfImage) { // no SoS marker found, treat all data as tokens
+                std.debug.print("{x} 0x{?} -> ", .{ i, std.fmt.fmtSliceHexLower(self.data[i .. i + 2]) });
+                std.debug.print("{?}\n", .{marker});
+            }
+
+            if (!soa_found) {
+                switch (marker) {
+                    .startOfImage => {
+                        try self.markers.put(marker, i);
+                    },
+                    .endOfImage => {
+                        break;
+                    },
+                    .defineRestartInterval => {
+                        restartInterval = std.mem.readIntSlice(u16, self.data[i + 2 .. i + 4], std.builtin.Endian.Big); // skip two bytes to find the length we need to skip
+                        i += 4; // NOTE: HACK
+                    },
+                    .startOfScan => { // found "Start Of Scan" marker, all following data is raw data, start brute-force search for end token
+                        soa_found = true;
+                    },
+                    .defineHuffmanTable => {
+                        i += 2;
+                        const block_length = std.mem.readIntSlice(u16, self.data[i .. i + 2], std.builtin.Endian.Big);
+                        const end_position = i + block_length;
+                        i += 2;
+
+                        while (true) {
+                            const table_class = @ptrCast(*const HuffmanTableHeader, &self.data[i]);
+                            i += 1;
+                            std.debug.print("HTH: {?}\n", .{table_class});
+
+                            const lengths: *const [16]u8 = @ptrCast(*const [16]u8, self.data[i .. i + 16]);
+                            i += 16;
+                            std.debug.print("Lengths: ", .{});
+
+                            utils.slicePrint(u8, lengths);
+                            std.debug.print("\n", .{});
+
+                            var sum: usize = 0;
+                            for (lengths) |len| {
+                                sum += len;
+                            }
+                            std.debug.print("Total element length: {d}\n", .{sum});
+
+                            var data_i: usize = 0;
+                            _ = data_i;
+                            var code_candidate: u16 = 0;
+                            _ = code_candidate;
+                            var code_index: usize = 0;
+                            _ = code_index;
+
+                            i += sum;
+
+                            if (i == end_position) break;
+
+                            // var code_map = std.AutoHashMap(u5, u16).init(allocator);
+                            // defer code_map.deinit();
+
+                            // while (code_index < 16) : (code_index += 1) {
+                            //     const code_count_for_index = lengths[code_index];
+                            //     var current_code_index: usize = 0;
+                            //     std.debug.print("{d} codes of length {d}: [", .{ code_count_for_index, code_index + 1 });
+                            //     while (current_code_index < code_count_for_index) : (current_code_index += 1) {
+                            //         std.debug.print("{b}", .{code_candidate});
+                            //         if (current_code_index != code_count_for_index - 1) std.debug.print(",", .{});
+                            //         code_candidate += 1;
+                            //     }
+                            //     std.debug.print("]\n", .{});
+
+                            //     code_candidate <<= 1; // shift to the left to add zero in front
+                            // }
+                        }
+
+                        i -= 2; // HACK: why this is needed?
+                    },
+                    else => {
+                        const block_length = std.mem.readIntSlice(u16, self.data[i + 2 .. i + 4], std.builtin.Endian.Big); // skip two bytes to find the length we need to skip
+                        // std.debug.print("block length: 0x{?} -> {d}\n", .{ std.fmt.fmtSliceHexLower(self.data[i + 2 .. i + 4]), block_length });
+                        i += block_length;
+                    },
+                }
+            }
+        }
     }
 };
 
@@ -72,98 +172,11 @@ pub fn main() !void {
 
     try file.reader().readNoEof(buffer);
 
-    var i: usize = 0;
-    var soa_found = false;
+    var parser: Parser = Parser.init(allocator, buffer);
+    defer parser.deinit();
 
-    var restartInterval: ?u16 = null;
-
-    while (i < buffer.len) : (i += 2) {
-        const value = std.mem.readIntSlice(u16, buffer[i .. i + 2], std.builtin.Endian.Big);
-        const marker = @intToEnum(Marker, value);
-
-        if (soa_found and marker == .endOfImage) {}
-
-        if (!soa_found or marker == .endOfImage) { // no SoS marker found, treat all data as tokens
-            std.debug.print("{x} 0x{?} -> ", .{ i, std.fmt.fmtSliceHexLower(buffer[i .. i + 2]) });
-            std.debug.print("{?}\n", .{marker});
-        }
-
-        if (!soa_found) {
-            switch (marker) {
-                .startOfImage => {},
-                .endOfImage => {
-                    break;
-                },
-                .defineRestartInterval => {
-                    restartInterval = std.mem.readIntSlice(u16, buffer[i + 2 .. i + 4], std.builtin.Endian.Big); // skip two bytes to find the length we need to skip
-                    i += 4; // NOTE: HACK
-                },
-                .startOfScan => { // found "Start Of Scan" marker, all following data is raw data, start brute-force search for end token
-                    soa_found = true;
-                },
-                .defineHuffmanTable => {
-                    i += 2;
-                    const block_length = std.mem.readIntSlice(u16, buffer[i .. i + 2], std.builtin.Endian.Big);
-                    const end_position = i + block_length;
-                    i += 2;
-
-                    while (true) {
-                        const table_class = @ptrCast(*HuffmanTableHeader, &buffer[i]);
-                        i += 1;
-                        std.debug.print("HTH: {?}\n", .{table_class});
-
-                        const lengths: *[16]u8 = @ptrCast(*[16]u8, buffer[i .. i + 16]);
-                        i += 16;
-                        std.debug.print("Lengths: ", .{});
-
-                        utils.slicePrint(u8, lengths);
-                        std.debug.print("\n", .{});
-
-                        var sum: usize = 0;
-                        for (lengths) |len| {
-                            sum += len;
-                        }
-                        std.debug.print("Total element length: {d}\n", .{sum});
-
-                        var data_i: usize = 0;
-                        _ = data_i;
-                        var code_candidate: u16 = 0;
-                        _ = code_candidate;
-                        var code_index: usize = 0;
-                        _ = code_index;
-
-                        i += sum;
-
-                        if (i == end_position) break;
-
-                        // var code_map = std.AutoHashMap(u5, u16).init(allocator);
-                        // defer code_map.deinit();
-
-                        // while (code_index < 16) : (code_index += 1) {
-                        //     const code_count_for_index = lengths[code_index];
-                        //     var current_code_index: usize = 0;
-                        //     std.debug.print("{d} codes of length {d}: [", .{ code_count_for_index, code_index + 1 });
-                        //     while (current_code_index < code_count_for_index) : (current_code_index += 1) {
-                        //         std.debug.print("{b}", .{code_candidate});
-                        //         if (current_code_index != code_count_for_index - 1) std.debug.print(",", .{});
-                        //         code_candidate += 1;
-                        //     }
-                        //     std.debug.print("]\n", .{});
-
-                        //     code_candidate <<= 1; // shift to the left to add zero in front
-                        // }
-                    }
-
-                    i -= 2; // HACK: why this is needed?
-                },
-                else => {
-                    const block_length = std.mem.readIntSlice(u16, buffer[i + 2 .. i + 4], std.builtin.Endian.Big); // skip two bytes to find the length we need to skip
-                    // std.debug.print("block length: 0x{?} -> {d}\n", .{ std.fmt.fmtSliceHexLower(buffer[i + 2 .. i + 4]), block_length });
-                    i += block_length;
-                },
-            }
-        }
-    }
+    try parser.parseMarkers();
+    std.debug.print("markers: {?}\n", .{parser.markers});
 
     // while (true) {
     //     const number_of_read_bytes = try buffered_file.read(&buffer);
--- src/utils.zig
+++ src/utils.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn slicePrint(comptime T: type, slice: []T) void {
+pub fn slicePrint(comptime T: type, slice: []const T) void {
     std.debug.print("[", .{});
     var i: usize = 0;
     while (i < slice.len) : (i += 1) {
@@ -8,4 +8,12 @@ pub fn slicePrint(comptime T: type, slice: []T) void {
         if (i != slice.len - 1) std.debug.print(", ", .{});
     }
     std.debug.print("]", .{});
+}
+
+pub fn mapPrint(
+    comptime K: type,
+    comptime V: type,
+) void {
+    _ = V;
+    _ = K;
 }
